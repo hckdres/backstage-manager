26de4d8686ce2b1cc02ce6d77848bd5320a2921c
santiago0217 <santiagojaviersilvacaliman@gmail.com>
2026-05-05 11:40:04 -0500

    Entidad de Analisis Financiero Completada con sus contructores y getters y setters

--- src/main/java/org/example/ax0006/Entity/AnalisisFinanciero.java
+++ src/main/java/org/example/ax0006/Entity/AnalisisFinanciero.java
@@ -1,5 +1,39 @@
 package org.example.ax0006.Entity;
 
 public class AnalisisFinanciero {
-    /*Pendiente a implementacion*/
+
+    private int idAnalisisF;
+    private int presupuesto;
+    private int gastos;
+
+    public AnalisisFinanciero() {}
+
+    public AnalisisFinanciero(int presupuesto, int gastos) {
+        this.presupuesto = presupuesto;
+        this.gastos = gastos;
+    }
+
+    public int getIdAnalisisF() {
+        return idAnalisisF;
+    }
+
+    public void setIdAnalisisF(int idAnalisisF) {
+        this.idAnalisisF = idAnalisisF;
+    }
+
+    public int getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(int presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public int getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(int gastos) {
+        this.gastos = gastos;
+    }
 }
