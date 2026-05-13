5d3cdf460ab01bcccda9ac5f3cbe932599d7c7ad
Martin <sanmiguel_md@javeriana.edu.co>
2026-05-12 22:00:01 -0500

    Se inicia el test del conciertoService

---
+++ src/test/java/org/example/ax0006/service/RolServiceTest.java
@@ -0,0 +1,24 @@
+package org.example.ax0006.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RolServiceTest {
+
+    @Test
+    void obtenerRolesAsignables() {
+    }
+
+    @Test
+    void obtenerUsuarios() {
+    }
+
+    @Test
+    void obtenerNombreRol() {
+    }
+
+    @Test
+    void actualizarRolGlobal() {
+    }
+}
