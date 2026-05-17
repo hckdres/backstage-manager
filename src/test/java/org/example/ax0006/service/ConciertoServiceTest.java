eb4b8ec56d7d1df94665fa04a03f972bac5a220b
Martin <sanmiguel_md@javeriana.edu.co>
2026-05-17 16:24:26 -0500

    Se hace la primera prueba de eliminar concierto

--- src/test/java/org/example/ax0006/service/ConciertoServiceTest.java
+++ src/test/java/org/example/ax0006/service/ConciertoServiceTest.java
@@ -180,10 +180,6 @@ class ConciertoServiceTest {
         }
 
         @Test
-        void aprobarConcierto() {
-        }
-
-        @Test
         void eliminarConcierto() {
         }
         @Test
@@ -191,7 +187,7 @@ class ConciertoServiceTest {
     }
 
     @Nested
-    @DisplayName("aprobarConcierto")
+    @DisplayName("AprobarConcierto")
     class aprobarConciertoMETODO{
         @Test
         void aprobarConcierto() {
@@ -455,6 +451,111 @@ class ConciertoServiceTest {
                        () -> assertNotNull(conciertoSolo.getHorario())
                );
            }
+
+        }
+    }
+
+    @Nested
+    @DisplayName("EliminarConcierto")
+    class EliminarConcierto {
+        @Test
+        void eliminarConcierto() {
+            Concierto concierto = new Concierto();
+            concierto.setNombreConcierto("Tour mundial LOCO");
+            concierto.setAforo(25600);
+
+            // ==========================================
+            // 1. HORARIO
+            // ==========================================
+            Horario horario = new Horario();
+            LocalDate fechaConcierto = LocalDate.of(2026, 12, 29);
+            horario.setFechaInicio(fechaConcierto);
+            horario.setFechaFin(fechaConcierto.plusDays(1));
+            horario.setHoraInicio(LocalTime.of(11, 0));
+            horario.setHoraFin(LocalTime.of(12, 0));
+
+            horarioRepo.guardar(horario);
+            horario.setIdHorario(1); // Garantizado al estar la BD limpia
+            concierto.setHorario(horario);
+
+            // ==========================================
+            // 2. ARTISTA
+            // ==========================================
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Juan Luis Guerra");
+            usuario.setGmail("juanluis@guerra.com");
+            usuario.setContrasena("password123");
+            usuario.setIdRol(3); // Rol de Manager/Artista
+
+            boolean artistaGuardado = usuarioRepo.guardar(usuario);
+            assertTrue(artistaGuardado, "El repositorio falló al guardar al artista.");
+
+            Usuario artistaReal = usuarioRepo.buscarPorNombre("Juan Luis Guerra");
+            assertNotNull(artistaReal, "No se pudo recuperar el artista de la base de datos.");
+
+            usuario.setIdUsuario(artistaReal.getIdUsuario());
+            concierto.setArtista(usuario);
+
+            // ==========================================
+            // 3. CONTRATO
+            // ==========================================
+            Contrato contrato = new Contrato();
+            contrato.setFecha(fechaConcierto.minusDays(10));
+
+            Clausula clausula = new Clausula();
+            clausula.setClausula("pago en efectivo");
+            List<Clausula> clausulas = new ArrayList<>();
+            clausulas.add(clausula);
+            contrato.setClausulas(clausulas);
+
+            int idContratoReal = contratoService.crearContrato(contrato);
+            assertTrue(idContratoReal > 0, "El servicio falló al crear el contrato.");
+
+            contrato.setIdContrato(idContratoReal);
+            concierto.setContrato(contrato);
+            concierto.setIdContrato(idContratoReal);
+
+            // ==========================================
+            // 4. EJECUTAR SERVICIO
+            // ==========================================
+            conciertoService.crearConcierto(concierto);
+
+            List<Concierto> conciertosGuardados = conciertoService.obtenerConciertosSolos();
+
+            // ==========================================
+            // 5. SE BUSCA EL CONCIERTO EN LA DB
+            // ==========================================
+            Concierto conciertoRecuperado = null;
+            for (Concierto c : conciertosGuardados) {
+                if ("Tour mundial LOCO".equals(c.getNombreConcierto())) {
+                    conciertoRecuperado = c;
+                    break;
+                }
+            }
+
+            assertNotNull(conciertoRecuperado, "el concierto antes de ser eliminado no es null");
+
+            conciertosGuardados = null;
+
+            conciertoService.eliminarConcierto(conciertoRecuperado.getIdConcierto(), conciertoRecuperado.getHorario().getIdHorario());
+            conciertosGuardados = conciertoService.obtenerConciertosSolos();
+
+            // ===============================================
+            // 6. SE BUSCA EL CONCIERTO ELIMINADO EN LA DB
+            // ===============================================
+            conciertoRecuperado = null;
+            for (Concierto c : conciertosGuardados) {
+                if ("Tour mundial LOCO".equals(c.getNombreConcierto())) {
+                    conciertoRecuperado = c;
+                    break;
+                }
+            }
+
+            // ==========================================
+            // 7. ASSERTS
+            // ==========================================
+
+            assertNull(conciertoRecuperado, "es null");
 
         }
     }
